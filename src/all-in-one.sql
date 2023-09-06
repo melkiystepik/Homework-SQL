3b9ecedcdf57f86fd6c33b58198209bd8ca54271
melkiystepik <stepigandruxer@gmail.com>
2023-09-06 14:37:54 +0300

    homework2, attempt4 - final) new BD: all tables, creation script, all tasks scripts in one file

--- src/all-in-one.sql
+++ src/all-in-one.sql
@@ -1,81 +1,98 @@
 ---1
-select model, speed, hd from pc where price < cast(500 as money);
+select model, speed, hd from pc where price < 500;
 ---2
--- may be too easy?)))
--- select maker from printer;
-select distinct maker from printer where type = 'printer';
+select distinct maker from product where type = 'printer';
 ---3
-select model, ram, screen from laptop where price > cast(1000 as money);
+select model, ram, screen from laptop where price > 1000;
 ---4
 select * from printer where color =  'y';
 ---5
-select model, speed, hd from pc where (cd = '12x' or cd = '24x') and price < cast(600 as money);
+select model, speed, hd from pc where (cd = '12x' or cd = '24x') and price < 600;
 ---6
-select maker, speed from laptop where hd >= 100;
+select product.maker, laptop.speed from laptop
+join product on product.model = laptop.model
+where (laptop.hd >= 100);
 ---7
-select model, price from pc where maker = 'Sony'
+select product.model, laptop.price from product
+join laptop on product.model = laptop.model
+where product.maker = 'Sony'
 union
-select model, price from laptop where maker = 'Sony'
+select product.model, pc.price from product
+join pc on product.model = pc.model
+where product.maker = 'Sony'
 union
-select model, price from printer where maker = 'Sony';
+select product.model, printer.price from product
+join printer on product.model = printer.model
+where product.maker = 'Sony';
 ---8
-select distinct pc.maker from pc where pc.maker not in (select maker from laptop);
+select distinct pcs.maker from (select maker from product where type = 'pc') as pcs
+where pcs.maker not in (select maker from product where type = 'laptop');
 ---9
-select distinct maker from pc where speed >= 450;
+select distinct maker from product
+join pc on product.model = pc.model
+where speed >= 450;
 ---10
 select model, price from printer where price = (select MAX(price) from printer);
 ---11
 select AVG(pc.speed) from pc;
 ---12
-select avg(laptop.speed) from laptop where price > cast(1000 as money);
+select avg(laptop.speed) from laptop where price > 1000;
 ---13
-select avg(speed) from pc where maker = 'Asus';
+select avg(speed) from pc
+join product on pc.model = product.model
+where maker = 'Asus';
 ---14
-select pc1.speed, avg(cast(pc1.price as numeric)) as avgprice from pc as pc1 join pc as pc2 on pc1.speed = pc2.speed group by pc1.speed;
+select distinct pc1.speed, avg(pc1.price) as avgprice from pc as pc1 join pc as pc2 on pc1.speed = pc2.speed group by pc1.speed;
 ---15
 SELECT hd FROM pc GROUP BY hd HAVING count(hd)>1;
 ---16
---here I had some difficalties
---select pc1.model, pc2.model, pc1.speed, pc1.ram
---from pc as pc1 left join pc as pc2 on pc1.model != pc2.model
---where pc1.speed=pc2.speed and pc1.ram = pc2.ram
---group by pc1.model, pc2.model, pc1.speed, pc1.ram
---order by pc1.model
---and later I was prompted for very elegant solution
 select pc1.model, pc2.model, pc1.speed, pc1.ram
 from pc as pc1
 join pc as pc2 on pc1.model > pc2.model
 where pc1.speed=pc2.speed and pc1.ram = pc2.ram
 order by pc1.model;
 ---17
-select distinct laptop.type, laptop.model, laptop.speed from laptop, pc where laptop.speed < pc.speed;
+select distinct laptop.model from laptop
+join pc on laptop.speed < pc.speed;
 ---18
-select maker, price from printer where price = (select MIN(price) from printer);
+select product.maker, printer.price from printer
+join product on printer.model = product.model
+where price = (select MIN(price) from printer);
 ---19
-select maker, avg(screen) from laptop group by maker;
+select product.maker, avg(laptop.screen) from laptop
+join product on laptop.model = product.model
+group by product.maker;
 ---20
-select maker, count(maker) from pc group by maker having count(maker) >= 3;
+select maker, count(maker) from product where type = 'pc' group by maker having count(maker) >= 3;
 ---21
-select maker, max(price) from pc group by maker;
+select product.maker, max(pc.price) from pc
+join product on pc.model = product.model group by maker;
 ---22
-select speed, avg(cast(price as numeric)) from pc where speed > 600 group by speed;
+select speed, avg(price) from pc where speed > 600 group by speed;
 ---23
-select Distinct pc.maker from pc join laptop on pc.maker = laptop.maker where laptop.speed >= 750 and pc.speed > 750;
+select distinct product.maker from product
+join laptop on product.model = laptop.model
+where laptop.speed >= 750
+union
+select distinct product.maker from product
+join pc on product.model = pc.model
+where pc.speed >= 750;
 ---24
-select produkt.model from produkt
-join pc on produkt.model = pc.model
+select product.model from product
+join pc on product.model = pc.model
 where pc.price = (select max(pc.price) from pc)
 union
-select produkt.model from produkt
-join printer on produkt.model = printer.model
+select product.model from product
+join printer on product.model = printer.model
 where printer.price = (select max(printer.price) from printer)
 union
-select produkt.model from produkt
-join laptop on produkt.model = laptop.model
+select product.model from product
+join laptop on product.model = laptop.model
 where laptop.price = (select max(laptop.price) from laptop);
 ---25
-select printer.maker from printer
-join pc on printer.maker = pc.maker
-where pc.ram = (select min(pc.ram) from pc)
-group by printer.maker, pc.speed
-having pc.speed = max(pc.speed);
+select printers.maker from (select * from product where product.type = 'printer') as printers
+join (select * from product where product.type = 'pc') as pcs on printers.maker = pcs.maker
+join pc on pc.model=pcs.model
+where  pc.speed = (select max(pc.speed) from pc)
+group by printers.maker, pc.ram
+having pc.ram = min(pc.ram);
